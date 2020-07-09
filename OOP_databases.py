cd34ba60eaba7d0bd46934221f72555762696f06
Lycurgus1 <max.palmer.official@outlook.com>
2020-07-09 16:29:14 +0100

    Python database notes v2

---
+++ OOP_databases.py
@@ -0,0 +1,10 @@
+# Create class
+
+# Create function to intialise connection
+
+# Creat function to intialise cursor
+
+# Create function to fetch many
+# Create function to fetch all
+# Create function to fetch one
+# User input from user to say which one they want
--- database_connections.py
+++ database_connections.py
@@ -5,17 +5,26 @@ import pyodbc
 # Goal is to establish connection & read data from database to the python console
 
 server = 'databases2.spartaglobal.academy'
+#databases2.spartaglobal.academy
 database = 'Northwind'
 username = 'SA'
 password = 'Passw0rd2018'
 
 
-# Specifying the ODBC driver, server name, database, etc. directly
-# ODBC = api for connecting to microsoft applications and servers. Open database connection
+# # Specifying the ODBC driver, server name, database, etc. directly
+# # ODBC = api for connecting to microsoft applications and servers. Open database connection
+
+# # Testing without using Try loop, due to erros
+# connections = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE=' + database +';UID='+ username +';PWD='+ password)
+# cursor = connections.cursor()
+# print(connections)
+# print(cursor)
+
 # Differiantes from documentation slightly
-connection_string = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password
+connection_string = ('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE=' + database +';UID='+ username +';PWD='+ password)
 
 # Trying to connect with database string. If not able to connect it times out in 5 seconds
+# def connection_loop(connection):
 try:
     with pyodbc.connect(connection_string, timeout = 20) as connection:
         print("connection did not time out")
@@ -25,29 +34,46 @@ else:
     # Get cursor from connection  to execute the codethrough
     # Using cursor.fetch to iterate through rows of data
     cursor = connection.cursor()
-    print(connection)
-    print(cursor)
-
-
-# # Using a DSN, but providing a password as well
-# cnxn = pyodbc.connect('DSN=test;PWD=password')
+    # print(connection)
+    # print(cursor)#
 #
-# # Create a cursor from the connection
-# cursor = cnxn.cursor()
+# connection_loop(connection_string)
 
-# import pyodbc
-# # server = 'databases2.spartaglobal.academy'
-# # database = 'Northwind'
-# # username = 'SA'
-# # password = 'Passw0rd2018'
-# #
-# # import pyodbc
-# server = 'databases2.spartaglobal.academy'
-# #databases2.spartaglobal.academy
-# database = 'Northwind'
-# username = 'SA'
-# password = 'Passw0rd2018'
-# connections = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-# cursor = connections.cursor()
-# print(connections)
-# print(cursor)
+# # Connecting to server
+# cnxn = pyodbc.connect(connection_string)
+# # Creating cursor
+# cursor = cnxn.cursor()
+# Executing SQL command
+query_result = cursor.execute("select * from Customers ")
+print(query_result)
+
+# Fetching all rows
+rows = cursor.fetchall()
+
+# Fetching multiple rows
+# rows = cursor.fetchmany(2)
+
+# # Fetching one row
+# rows = cursor.fetchone()
+
+# # Print methods
+# # Printing rows using for loop
+# for row in rows:
+#     print(row)
+
+# # Printing row in rows, getting row info from column1 and contact name category for each row
+# for row in rows:
+#     print("Column1 :", row[0])
+#     print("ContactName :", row.ContactName)
+
+# # Printing 1st row in table
+# print(rows[0])
+#
+# # Printing value for column name. Can only do so if printing one row
+# print(rows.CompanyName)
+
+
+
+
+
+
